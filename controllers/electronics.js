6b388798c34384383296ddbbc8ee065503e78614
tallam-git <tallamcharanteja99@gmail.com>
2023-04-13 22:22:50 -0500

    Updated Files according to lab-12

--- controllers/electronics.js
+++ controllers/electronics.js
@@ -12,8 +12,15 @@ exports.electronic_list = async function (req, res) {
     }
 };
 // for a specific Electronic.
-exports.electronic_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: Electronic detail: ' + req.params.id);
+exports.electronic_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await Electronic.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 // Handle Electronic create on POST.
 exports.electronic_create_post = function (req, res) {
@@ -24,9 +31,25 @@ exports.electronic_delete = function (req, res) {
     res.send('NOT IMPLEMENTED: Electronic delete DELETE ' + req.params.id);
 };
 // Handle Electronic update form on PUT.
-exports.electronic_update_put = function (req, res) {
-    res.send('NOT IMPLEMENTED: Electronic update PUT' + req.params.id);
-};
+exports.electronic_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body
+    ${JSON.stringify(req.body)}`)
+    try {
+    let toUpdate = await Electronic.findById( req.params.id)
+    // Do updates of properties
+    if(req.body.Electronic_Name)
+    toUpdate.Electronic_Name = req.body.Electronic_Name;
+    if(req.body.Electronic_Origin) toUpdate.Electronic_Origin = req.body.Electronic_Origin;
+    if(req.body.Electronic_ManufacturingYear) toUpdate.Electronic_ManufacturingYear = req.body.Electronic_ManufacturingYear;
+    let result = await toUpdate.save();
+    console.log("Sucess " + result)
+    res.send(result)
+    } catch (err) {
+    res.status(500)
+    res.send(`{"error": ${err}: Update for id ${req.params.id}
+    failed`);
+    }
+    };
 exports.electronic_view_all_Page = async function (req, res) {
     try {
         theElectronics = await Electronic.find();
