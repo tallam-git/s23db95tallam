38de73e4f896146deff2557804b82fb476d594f2
tallam-git <tallamcharanteja99@gmail.com>
2023-04-18 21:37:49 -0500

    Updated Files

--- controllers/electronics.js
+++ controllers/electronics.js
@@ -27,29 +27,37 @@ exports.electronic_create_post = function (req, res) {
     res.send('NOT IMPLEMENTED: Electronic create POST');
 };
 // Handle Electronic delete form on DELETE.
-exports.electronic_delete = function (req, res) {
-    res.send('NOT IMPLEMENTED: Electronic delete DELETE ' + req.params.id);
+exports.electronic_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await Electronic.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
 // Handle Electronic update form on PUT.
-exports.electronic_update_put = async function(req, res) {
+exports.electronic_update_put = async function (req, res) {
     console.log(`update on id ${req.params.id} with body
     ${JSON.stringify(req.body)}`)
     try {
-    let toUpdate = await Electronic.findById( req.params.id)
-    // Do updates of properties
-    if(req.body.Electronic_Name)
-    toUpdate.Electronic_Name = req.body.Electronic_Name;
-    if(req.body.Electronic_Origin) toUpdate.Electronic_Origin = req.body.Electronic_Origin;
-    if(req.body.Electronic_ManufacturingYear) toUpdate.Electronic_ManufacturingYear = req.body.Electronic_ManufacturingYear;
-    let result = await toUpdate.save();
-    console.log("Sucess " + result)
-    res.send(result)
+        let toUpdate = await Electronic.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.Electronic_Name)
+            toUpdate.Electronic_Name = req.body.Electronic_Name;
+        if (req.body.Electronic_Origin) toUpdate.Electronic_Origin = req.body.Electronic_Origin;
+        if (req.body.Electronic_ManufacturingYear) toUpdate.Electronic_ManufacturingYear = req.body.Electronic_ManufacturingYear;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
     } catch (err) {
-    res.status(500)
-    res.send(`{"error": ${err}: Update for id ${req.params.id}
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id}
     failed`);
     }
-    };
+};
 exports.electronic_view_all_Page = async function (req, res) {
     try {
         theElectronics = await Electronic.find();
@@ -58,6 +66,18 @@ exports.electronic_view_all_Page = async function (req, res) {
     catch (err) {
         res.status(500);
         res.send(`{"error": ${err}}`);
+    }
+};
+exports.electronic_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await Electronic.findById(req.query.id)
+        res.render('electronicdetail',
+            { title: 'Electronic Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
     }
 };
 exports.electronic_create_post = async function (req, res) {
@@ -79,4 +99,40 @@ exports.electronic_create_post = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 };
+exports.electronic_create_Page = function (req, res) {
+    console.log("create view")
+    try {
+        res.render('electroniccreate', { title: 'Electronic Create' });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
 
+exports.electronic_update_Page = async function (req, res) {
+    console.log("update view for item " + req.query.id)
+    try {
+        let result = await Electronic.findById(req.query.id)
+        res.render('electronicupdate', { title: 'Electronic Update', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+exports.electronic_delete_Page = async function (req, res) {
+    console.log("Delete view for id " + req.query.id)
+    try {
+        result = await Electronic.findById(req.query.id)
+        res.render('electronicdelete', {
+            title: 'Electronic Delete', toShow:
+                result
+        });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
--- routes/electronics.js
+++ routes/electronics.js
@@ -4,5 +4,9 @@ var router = express.Router();
 /* GET home page. */
 const electronic_controlers= require('../controllers/electronics');
 router.get('/', electronic_controlers.electronic_view_all_Page);
-
+router.get('/detail', electronic_controlers.electronic_view_one_Page);
+/* GET create electronic page */
+router.get('/create', electronic_controlers.electronic_create_Page);
+router.get('/update', electronic_controlers.electronic_update_Page);
+router.get('/delete', electronic_controlers.electronic_delete_Page);
 module.exports = router;
